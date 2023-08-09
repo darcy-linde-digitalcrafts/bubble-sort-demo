73d51f3b83fd1f137f3b5aaca2812b52f38349df
Darcy Linde <darcy.linde@digitalcrafts.com>
2023-08-09 16:03:25 -0400

    Remove silly swap boolean

--- bubble.js
+++ bubble.js
@@ -1,8 +1,7 @@
 const bubbleSort = (input) => {
   for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length - 1 - i; j++) {
-      const swap = input[j] > input[j + 1];
-      if (swap) {
+      if (input[j] > input[j + 1]) {
         const temp = input[j + 1];
         input[j + 1] = input[j];
         input[j] = temp;
